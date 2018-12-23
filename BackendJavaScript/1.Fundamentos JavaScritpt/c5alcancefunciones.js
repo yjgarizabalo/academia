2799c858a1a94306bd5e0dbf93cf2c92f4c1babe
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-23 13:46:24 -0500

    update alcance funciones

---
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c5alcancefunciones.js
@@ -0,0 +1,10 @@
+// clase #5 Alcance de las funciones
+
+ var nombre = 'Emanuel'
+
+ function imprimernombreMayuscala() {
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+ }
+
+ imprimernombreMayuscala()
