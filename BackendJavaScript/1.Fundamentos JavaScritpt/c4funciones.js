57ae5ce8eef590f89c3404767f0a9de2c5e7e15a
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-23 12:47:25 -0500

    update funciones

---
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c4funciones.js
@@ -0,0 +1,11 @@
+// clase #4 funciones
+
+ var nombre = 'Emanuel', edad = 0
+
+ function imprimirEdad(n, e) {
+   console.log(`${n} Tiene ${e} años`)
+ }
+
+ imprimirEdad(nombre, edad)
+ imprimirEdad('Wendy', 26)
+ imprimirEdad('Yair', 26)
