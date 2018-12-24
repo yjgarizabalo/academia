42b6d078a77fa8433b2937b3374c1a9bbbd5c4ea
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-24 11:16:55 -0500

    update parametros

--- BackendJavaScript/1.Fundamentos JavaScritpt/c7desobjetos.js
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c7desobjetos.js
@@ -2,13 +2,13 @@
 
 var Emanuel = {
     nombre: 'Emanuel',
-    peso: '3',
+    Apellido: 'Garizabalo',
     edad: '0',
 }
 
 var Yair =  {
     nombre: 'Yair',
-    peso: '75',
+    peso: 'Garizabalo',
     edad: '26',
 }
 
@@ -16,10 +16,11 @@ function imprimirNombre(persona) {
     // var perosna = persona.nombre
     var {nombre} = persona
     var {edad} = persona
-    console.log(nombre, edad)
+    console.log(`Tu nombre es ${nombre} y tienes ${edad} años`)
 }
 
 imprimirNombre(Emanuel)
 imprimirNombre(Yair)
+
 
 
--- BackendJavaScript/1.Fundamentos JavaScritpt/c8parametros.js
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c8parametros.js
@@ -1,1 +1,35 @@
 // Clase #8 Parametros como Referencia o como Valor
+
+
+var Emanuel = {
+    nombre: 'Emanuel',
+    Apellido: 'Garizabalo',
+    edad: '0',
+}
+
+var Yair =  {
+    nombre: 'Yair',
+    peso: 'Garizabalo',
+    edad: '26',
+}
+
+function imprimirNombre(persona) {
+    // var perosna = persona.nombre
+    var {nombre} = persona
+    var {edad} = persona
+    console.log(`Tu nombre es ${nombre} y tienes ${edad} años`)
+}
+
+imprimirNombre(Emanuel)
+imprimirNombre(Yair)
+
+
+function cumpleAnos(persona) {
+    return {
+        ...persona,
+        edad: persona.edad + 1
+    }
+    //persona.edad += 1
+}
+
+
