5533d418aaaeb94292be9742ff153b73579016ec
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-25 09:35:51 -0500

    update comparaciones

---
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c9comparacion.js
@@ -0,0 +1,16 @@
+// Clase #9  Comparaciones
+
+var x = 7, y = '7'
+
+x === y
+
+// Para una mejor comparacion es mejor utilizar el triple igual "==="
+
+var Emanuel = {
+    nombre: 'Emanuel'
+}
+
+var Wendy = {
+    nombre: 'Wendy'
+}
+
