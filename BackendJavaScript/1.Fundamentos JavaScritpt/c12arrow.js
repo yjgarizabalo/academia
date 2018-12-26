c52bb32f31cc0ca729c6f1c5347156304f5cb5b1
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-25 21:29:35 -0500

    update arrow

--- BackendJavaScript/1.Fundamentos JavaScritpt/c12arrow.js
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c12arrow.js
@@ -1,1 +1,44 @@
 // Clase #12 Arrow Fuctions
+
+var Emanuel = {
+    nombre: 'Emanuel',
+    apellido: 'Garizabalo',
+    edad: 0,
+    ingeniero: true,
+    cocinero: false,
+    dentista: false,
+    futbolista: true,
+}
+
+var Maria = {
+    nombre:  'Maria',
+    apellido: 'Guzman',
+    edad: '18',
+}
+
+const MAYORIA_DE_EDAD = 18
+
+///function esmayorDeEedad(persona) {
+//    return persona.edad >= MAYORIA_DE_EDAD
+//}
+
+const esmayorDeEdad = ({edad}) => edad >= MAYORIA_DE_EDAD
+const esmenorDeEdad = ({edad}) => edad <= MAYORIA_DE_EDAD
+
+function imprimirsiMayorDeEdad(persona) {
+    if (!esmayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es mayor de edad`)
+    }
+    else {
+        console.log(`${persona.nombre} es menor de edad`) 
+    }
+    
+}
+
+
+function permitirAcceso(persona) {
+    if (!esmayorDeEdad(persona)) {
+        console.log('ACCESO DENEGADO')
+    }
+    
+}
