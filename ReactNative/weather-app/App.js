dcd049d49503dcde37065924aa0fdb7b8d095479
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-26 19:48:05 -0500

    update weather-app

--- ReactNative/weather-app/App.js
+++ ReactNative/weather-app/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style>San Francisco</Text>
-        <Text>Clear</Text>
-        <Text>15°</Text>
+        <Text style={styles.largText}>San Francisco</Text>
+        <Text style={styles.smallText}>Clear</Text>
+        <Text style={styles.smallText}>15°</Text>
       </View>
     );
   }
@@ -20,6 +21,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  largText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textStyle: {
+    fontFamily: Platform.OS === 'ios' ?  'AvenirNext-Regular' : 'Roboto'
+  }
 });
 
 
