c728b416c108227984b518726367b39bcff68242
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-28 06:49:39 -0500

    update weather-app

--- ReactNative/weather-app/App.js
+++ ReactNative/weather-app/App.js
@@ -24,10 +24,11 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { location } = this.state
     return (   
       <KeyboardAvoidingView  style={styles.container} behavior="padding">
        <ImageBackground source={bgImage} style={styles.background}>
-        <Text style={styles.largText}>San Francisco</Text>
+        <Text style={styles.largText}>{location}</Text>
         <Text style={styles.smallText}>Clear</Text>
         <Text style={styles.largText}>15°</Text>
         <SearchInput placeholder='Buscar Ciudad'
