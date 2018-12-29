880f7ea064a9cfd2e7e9ba1e5d218a7806497093
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-29 09:18:10 -0500

    add dowhile

---
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c15dowhile.js
@@ -0,0 +1,11 @@
+// Clase #15 DoWhile
+
+var contador = 0
+const llueve = () => Math.random() < 0.25
+
+
+ do {
+  contador++
+ } while (!llueve()) 
+
+console.log(`Fui a ver si llovia ${contador} veces`)
