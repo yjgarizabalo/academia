603668a8f9c63281ae718df13c20736f77a0610a
Yair Garizabalo <yjgarizabalo@gmail.com>
2018-12-29 09:52:53 -0500

    add switch

---
+++ BackendJavaScript/1.Fundamentos JavaScritpt/c16switch.js
@@ -0,0 +1,15 @@
+// Clase #16 Switch
+
+var equipo = prompt('Cual es tu equipo favorita del futbol Colombiano de primera division?')
+
+
+switch (equipo) {
+    case 'Alianza Petrolera':
+    case 'alianza petrolera':
+    console.log('Bandera de Santander (Colombia) Alianza Petrolera	Bandera de Colombia César Torres	Barrancabermeja	Daniel Villa Zapata	10.400')
+    break
+
+    default:
+    console.log ('No se encontro su equipo')
+    break
+}
